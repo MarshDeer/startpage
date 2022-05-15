7387a2d586a7a372eb1ad5126ea016405f757b4f
MarshDeer <marshdeer97@gmail.com>
2022-05-15 16:44:44 -0400

    removed redundant js

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,6 @@
 const body = document.querySelector('body');
-const main = document.querySelector('main');
 
 function themetoggle() {
-        main.classList.toggle('dark');
-        main.classList.toggle('light');
         body.classList.toggle('dark');
         body.classList.toggle('light');
 }
