091e93785fa5f7303845fd25014fb99a503b0aac
MarshDeer <marshdeer97@gmail.com>
2023-02-20 17:44:26 -0300

    Theme now checks for user preferences

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,17 @@
-const body = document.querySelector('body');
-
 function themetoggle() {
-        body.classList.toggle('dark');
-        body.classList.toggle('light');
+	if (document.documentElement.classList.contains("dark")) {
+		document.documentElement.classList.remove("dark");
+		document.documentElement.classList.add("light");
+	} else if (document.documentElement.classList.contains("light")) {
+		document.documentElement.classList.remove("light");
+		document.documentElement.classList.add("dark");
+	} else {
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.add("light");
+		}
+	}
 }
+
+themetoggle();
